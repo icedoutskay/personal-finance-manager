d35074e354942d2cfb9594d484e2d4a01d6832e0
icedoutskay <somkeneodikpo@gmail.com>
2025-04-18 14:58:19 +0100

    added backend url

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:3000/api/v1/";
+const BASE_URL = "https://personal-finance-manager-tryk.onrender.com";
 
 
 const GlobalContext = React.createContext()
