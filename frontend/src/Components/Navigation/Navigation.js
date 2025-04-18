2bf0ad6486cb439ec94c728bd6689873f592722c
icedoutskay <somkeneodikpo@gmail.com>
2025-04-18 16:12:59 +0100

    modified images

--- frontend/src/Components/Navigation/Navigation.js
+++ frontend/src/Components/Navigation/Navigation.js
@@ -11,7 +11,7 @@ function Navigation({active, setActive}) {
             <div className="user-con">
                 <img src={avatar} alt="" />
                 <div className="text">
-                    <h2>Somkene</h2>
+                    <h2>Emeka</h2>
                     <p>Your Money</p>
                 </div>
             </div>
--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "https://personal-finance-manager-tryk.onrender.com";
+const BASE_URL = "https://personal-finance-manager-tryk.onrender.com/";
 
 
 const GlobalContext = React.createContext()
